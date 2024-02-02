cc5d99afb10025458f6e9462c4377ea81ff5f6b4
Grégory <gregory.eyraud@gmail.com>
2024-02-02 10:56:45 +0100

    Ajout d'affichages statistiques sur le nombre de déplacement d'un attaquant (moyenne, médianne, écart type, minimum, maximum, mode et fréquence du mode)

--- modelling/main.py
+++ modelling/main.py
@@ -1,23 +1,35 @@
 from modelling import Modelling
 from attacker import Attacker
 import numpy as np
+from scipy import stats
 
 
 if __name__ == '__main__':
 
-    attacker = Attacker(M=5, K=12, know_M=False, is_uniform=True)
+    M = 4
+    K = 30
 
-    attacker.model.plot_graph()
+    attacker = Attacker(M=M, K=K, know_M=False, is_uniform=True)
+
+    # attacker.model.plot_graph()
 
     # Pour voir l'animation d'une attaque
     # attacker.animate_attack(interval=100)
 
-    """
-    # On lance nb_tries attaque
-    nb_tries = 100
+
+    # On lance nb_tries attaques
+    nb_tries = 10000
     tries = np.zeros(nb_tries)
     for i in range(0, nb_tries):
+        if ((i / nb_tries) * 100) % 10 == 0:
+            print(f"Progression : {(i / nb_tries) * 100}%")
         tries[i] = attacker.attack()
 
-    print(tries.mean())  # Nombre moyen de déplacement avant d'arriver aux données sensibles
-    """
+    print(f"Pour M = {M}, K = {K} et une probabilité de retour de 0.6 : ")
+    print(f"Nombre moyen de déplacement : {tries.mean()}")
+    print(f"Nombre médian de déplacement : {np.median(tries)}")
+    print(f"Écart type : {tries.std()}")
+    print(f"Nombre minimal de déplacement : {np.min(tries)}")
+    print(f"Nombre maximal de déplacement : {np.max(tries)}")
+    print(f"Mode : {stats.mode(tries, keepdims=True).mode[0]}")
+    print(f"Fréquence du mode : {stats.mode(tries, keepdims=True).count[0]}")
